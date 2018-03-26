2df6402afefd4773caef007cca5edc129888ca26
dirga ambara <swamsid@gmail.com>
2018-03-26 17:39:38 +0700

    aaa

--- perwita/app/Http/Controllers/master_keuangan/laporan/laporan_neraca_saldo.php
+++ perwita/app/Http/Controllers/master_keuangan/laporan/laporan_neraca_saldo.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Controller;
 class laporan_neraca_saldo extends Controller
 {
     public function index_neraca_saldo(Request $request, $throttle){
+
       return view("laporan_neraca_saldo.index")
               ->withRequest($request)
               ->withThrottle($throttle);
