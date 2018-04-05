faa6c94ad2d09b99bd9860ba6a48a3ca335ca327
xexehon <anafrdna@gmail.com>
2018-04-05 10:27:31 +0700

    test

--- perwita/resources/views/partials/_sidebar.blade.php
+++ perwita/resources/views/partials/_sidebar.blade.php
@@ -57,9 +57,9 @@
                             <a href="{{ url('setting/groupbaru')}}"><i class="fa fa-folder-open-o" aria-hidden="true"></i> Group Baru </a>
                         </li>
 
-                        <li class="sidebar {{Request::is('setting/daftarmenu') ? 'active' : '' || Request::is('setting/daftarmenu/*') ? 'active' : '' }} ">
+                       <!--  <li class="sidebar {{Request::is('setting/daftarmenu') ? 'active' : '' || Request::is('setting/daftarmenu/*') ? 'active' : '' }} ">
                             <a href="{{ url('setting/daftarmenu')}}"><i class="fa fa-folder-open-o" aria-hidden="true"></i> Daftar Menu </a>
-                        </li>
+                        </li> -->
 
                         <li>
                                 <a href="#"><i class="fa fa-folder-o" aria-hidden="true"></i> Keuangan <span class="fa arrow"></span></a>
