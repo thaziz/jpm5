bda2554d6a778ae73b6620ff6a53d7fe670eb862
xexehon <anafrdna@gmail.com>
2018-04-06 15:16:13 +0700

    test

--- perwita/app/Http/Controllers/MasterPurchaseController.php
+++ perwita/app/Http/Controllers/MasterPurchaseController.php
@@ -863,13 +863,14 @@ class MasterPurchaseController extends Controller
 		$cabang = $request->cabang;
 		
 		$supplier = DB::select("select * from supplier where idcabang = '$cabang'  order by idsup desc limit 1");
-		return json_encode($supplier);
+		
 		
 		if(count($supplier) > 0) {
 	//		return $fpg[0]->fpg_nofpg;
 			$explode = explode("/", $supplier[0]->no_supplier);
 			$idsupplier1 = $explode[2];
-		
+			
+			return json_encode($idsupplier1);
 
 			$idsupplier = (int)$idsupplier1 + 1;
 			$data['idsupplier'] = str_pad($idsupplier, 6, '0', STR_PAD_LEFT);
