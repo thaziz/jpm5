a69119bed353d49b08123556c713c8fe8069a5aa
adiyani17a <wielijarni@gmail.com>
2018-04-10 14:45:53 +0700

    as

--- perwita/resources/views/sales/do_kargo/print.blade.php
+++ perwita/resources/views/sales/do_kargo/print.blade.php
@@ -310,15 +310,12 @@
 					<th class="textcenter bot right top" width="20%">Nama</th>
 					<th class="textcenter bot right top" width="40%">Keterangan</th>
 					<th class="textcenter bot right top" width="15%">Kuantum</th>
-					<th class="textcenter bot right top"width="15%">Harga</th>
-					
 				</tr>
 				<tr>
 					<td class="bot right textcenter" height="25">1</td>
 					<td class="bot right textcenter">KARGO</td>
 					<td class="bot right textcenter">{{$nota[0]->no_surat_jalan}} - PENGIRIMAN KARGO DARI {{$nota[0]->asal}} KE {{$nota[0]->tujuan}} {{$nota[0]->nopol}}</td>
 					<td class="bot right textcenter">{{ number_format($nota[0]->jumlah, 0, ",", ".") }} {{$nota[0]->kode_satuan}}</td>
-					<td class="bot textright">{{ number_format($nota[0]->total, 0, ",", ".") }}</td>
 				</tr>
                 
 				
