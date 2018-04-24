d69e3263d57e05a8255c54a4e3e8c6215624c63b
AzrieL/Muttq <denyprasetyo41@gmail.com>
2018-04-24 13:11:11 +0700

    ee

--- perwita/app/Http/routes.php
+++ perwita/app/Http/routes.php
@@ -1167,25 +1167,25 @@ Route::get('sales/deliveryordercabangtracking/table','trackingdoController@getda
 Route::get('sales/deliveryordercabangtracking/autocomplete','trackingdoController@autocomplete');
 Route::get('sales/deliveryordercabangtracking/getdata/{nomor}','trackingdoController@getdata');
 // delivery order
-Route::get('sales/deliveryorder', 'sales\do_controller@index');
-Route::get('cetak_deliveryorderform/cetak_deliveryorderform', 'sales\do_controller@cetak_form');
-Route::get('sales/deliveryorderform', 'sales\do_controller@form');
-Route::get('sales/deliveryorderform/{nomor}/edit', 'sales\do_controller@form');
-Route::get('sales/deliveryorderform/tabel_data_detail', 'sales\do_controller@table_data_detail');
-Route::get('sales/deliveryorderform/get_data_detail', 'sales\do_controller@get_data_detail');
-Route::get('sales/deliveryorderform/tabel_item', 'sales\do_controller@table_data_item');
-Route::get('sales/deliveryorderform/get_item', 'sales\do_controller@get_item');
-Route::get('sales/deliveryorderform/cari_harga', 'sales\do_controller@cari_harga');
-Route::get('sales/deliveryorderform/cari_customer', 'sales\do_controller@cari_customer');
+// Route::get('sales/deliveryorder', 'sales\do_controller@index');
+// Route::get('cetak_deliveryorderform/cetak_deliveryorderform', 'sales\do_controller@cetak_form');
+// Route::get('sales/deliveryorderform', 'sales\do_controller@form');
+// Route::get('sales/deliveryorderform/{nomor}/edit', 'sales\do_controller@form');
+// Route::get('sales/deliveryorderform/tabel_data_detail', 'sales\do_controller@table_data_detail');
+// Route::get('sales/deliveryorderform/get_data_detail', 'sales\do_controller@get_data_detail');
+// Route::get('sales/deliveryorderform/tabel_item', 'sales\do_controller@table_data_item');
+// Route::get('sales/deliveryorderform/get_item', 'sales\do_controller@get_item');
+// Route::get('sales/deliveryorderform/cari_harga', 'sales\do_controller@cari_harga');
+// Route::get('sales/deliveryorderform/cari_customer', 'sales\do_controller@cari_customer');
 
-Route::post('sales/deliveryorderform/save_data', 'sales\do_controller@save_data');
-Route::get('sales/deliveryorderform/save_data', 'sales\do_controller@save_data');
-Route::post('sales/deliveryorderform/save_data_detail', 'sales\do_controller@save_data_detail');
-Route::get('sales/deliveryorderform/{nomor}/hapus_data', 'sales\do_controller@hapus_data');
-Route::post('sales/deliveryorderform/hapus_data_detail', 'sales\do_controller@hapus_data_detail');
-Route::get('sales/deliveryorderform/{nomor}/update_status', 'sales\do_controller@form_update_status');
-Route::post('sales/deliveryorderform/save_update_status', 'sales\do_controller@save_update_status');
-Route::get('sales/deliveryorderform/{nomor}/nota', 'sales\do_controller@cetak_nota');
+// Route::post('sales/deliveryorderform/save_data', 'sales\do_controller@save_data');
+// Route::get('sales/deliveryorderform/save_data', 'sales\do_controller@save_data');
+// Route::post('sales/deliveryorderform/save_data_detail', 'sales\do_controller@save_data_detail');
+// Route::get('sales/deliveryorderform/{nomor}/hapus_data', 'sales\do_controller@hapus_data');
+// Route::post('sales/deliveryorderform/hapus_data_detail', 'sales\do_controller@hapus_data_detail');
+// Route::get('sales/deliveryorderform/{nomor}/update_status', 'sales\do_controller@form_update_status');
+// Route::post('sales/deliveryorderform/save_update_status', 'sales\do_controller@save_update_status');
+// Route::get('sales/deliveryorderform/{nomor}/nota', 'sales\do_controller@cetak_nota');
 
 
 
@@ -1194,24 +1194,24 @@ Route::get('sales/deliveryorderform/{nomor}/nota', 'sales\do_controller@cetak_no
 ////////////
 
 
-// Route::get('sales/deliveryorder', 'sales\do_Controller@index');
-// Route::get('sales/deliveryorderform', 'sales\do_Controller@form');
-// Route::get('sales/deliveryorderform/{nomor}/edit', 'sales\do_Controller@form');
-// Route::get('sales/deliveryorderform/tabel_data_detail', 'sales\do_Controller@table_data_detail');
-// Route::get('sales/deliveryorderform/get_data_detail', 'sales\do_Controller@get_data_detail');
-// Route::get('sales/deliveryorderform/tabel_item', 'sales\do_Controller@table_data_item');
-// Route::get('sales/deliveryorderform/get_item', 'sales\do_Controller@get_item');
-// Route::get('sales/deliveryorderform/cari_harga', 'sales\do_Controller@cari_harga');
-// Route::get('sales/deliveryorderform/cari_customer', 'sales\do_Controller@cari_customer');
+Route::get('sales/deliveryorder', 'sales\do_Controller@index');
+Route::get('sales/deliveryorderform', 'sales\do_Controller@form');
+Route::get('sales/deliveryorderform/{nomor}/edit', 'sales\do_Controller@form');
+Route::get('sales/deliveryorderform/tabel_data_detail', 'sales\do_Controller@table_data_detail');
+Route::get('sales/deliveryorderform/get_data_detail', 'sales\do_Controller@get_data_detail');
+Route::get('sales/deliveryorderform/tabel_item', 'sales\do_Controller@table_data_item');
+Route::get('sales/deliveryorderform/get_item', 'sales\do_Controller@get_item');
+Route::get('sales/deliveryorderform/cari_harga', 'sales\do_Controller@cari_harga');
+Route::get('sales/deliveryorderform/cari_customer', 'sales\do_Controller@cari_customer');
 
-// Route::post('sales/deliveryorderform/save_data', 'sales\do_Controller@save_data');
-// Route::get('sales/deliveryorderform/save_data', 'sales\do_Controller@save_data');
-// Route::post('sales/deliveryorderform/save_data_detail', 'sales\do_Controller@save_data_detail');
-// Route::get('sales/deliveryorderform/{nomor}/hapus_data', 'sales\do_Controller@hapus_data');
-// Route::post('sales/deliveryorderform/hapus_data_detail', 'sales\do_Controller@hapus_data_detail');
-// Route::get('sales/deliveryorderform/{nomor}/update_status', 'sales\do_Controller@form_update_status');
-// Route::post('sales/deliveryorderform/save_update_status', 'sales\do_Controller@save_update_status');
-// Route::get('sales/deliveryorderform/{nomor}/nota', 'sales\do_Controller@cetak_nota');
+Route::post('sales/deliveryorderform/save_data', 'sales\do_Controller@save_data');
+Route::get('sales/deliveryorderform/save_data', 'sales\do_Controller@save_data');
+Route::post('sales/deliveryorderform/save_data_detail', 'sales\do_Controller@save_data_detail');
+Route::get('sales/deliveryorderform/{nomor}/hapus_data', 'sales\do_Controller@hapus_data');
+Route::post('sales/deliveryorderform/hapus_data_detail', 'sales\do_Controller@hapus_data_detail');
+Route::get('sales/deliveryorderform/{nomor}/update_status', 'sales\do_Controller@form_update_status');
+Route::post('sales/deliveryorderform/save_update_status', 'sales\do_Controller@save_update_status');
+Route::get('sales/deliveryorderform/{nomor}/nota', 'sales\do_Controller@cetak_nota');
 
 
 
--- perwita/resources/views/purchase/master/master_penjualan/laporan/lap_piutang.blade.php
+++ perwita/resources/views/purchase/master/master_penjualan/laporan/lap_piutang.blade.php
@@ -11,7 +11,23 @@
 
   }
   .dataTables_filter, .dataTables_info { display: none; }
-
+  .saldo{
+    border: none;
+    background-color: #e6ffda;
+    color: #676a6c;
+  }
+  #total_debet{
+    border: none;
+    color: #676a6c;
+  }
+  #total_kredit{
+    border: none;
+    color: #676a6c;
+  }
+  #total_total{
+    border: none;
+    color: #676a6c;
+  }
 </style>
 <div class="wrapper wrapper-content animated fadeInRight">
     <div class="row">
@@ -75,36 +91,67 @@
                     </div>
                 </form>
                 <div class="box-body">
-                <table id="addColumn" class="table table-bordered table-striped">
+                <table id="addColumn" class="table table-bordered table-striped" width="100%">
                     <thead>
                         <tr>
-                            <th> Kode</th>
-                            <th> Tgl </th>
-                            <th> Keterangan </th>
-                            <th> Debet(+) </th>
-                            <th> Kredit(-) </th>
-                            <th> saldo </th>
+                            <th width="10%"> Kode</th>
+                            <th width="10%"> Tgl </th>
+                            <th width="10%"> Keterangan </th>
+                            <th width="10%"> Debet(+) </th>
+                            <th width="10%"> Kredit(-) </th>
+                            <th width="10%"> saldo </th>
                         </tr>
                     </thead>
                     <tbody>
                       @foreach ($data_i as $a => $element)
                          <tr>
-                           <td colspan="10">{{ $data_i[$a]->i_kode_customer }}</td>
+                           <td colspan="6">{{ $data_i[$a]->i_kode_customer }}</td>
                          </tr>
-                         
+                         @foreach ($data as $e => $element)
                       <tr style="text-align: right;background-color: #e6ffda;">
-                         
-                           @foreach ($data_p as $e => $element)
-                            @if ($data_i[$a]->i_kode_customer == $data_p[$e]->i_kode_customer)
-                                  <td>{{ $data_p[$e]->i_kode_customer }}</td>
-                                  <td>{{ $data_p[$e]->i_kode_customer }}</td>
-                                  <td>{{ $data_p[$e]->i_kode_customer }}</td>
-                                  <td>{{ $data_p[$e]->i_kode_customer }}</td>
-                            @endif
-                           
-                        @endforeach
+                          
+                            @if ($data_i[$a]->i_kode_customer == $data[$e]->cutomer)
+                                    <td><input type="hidden" value="{{ $data[$e]->kode }}" name="nomor">{{ $data[$e]->kode }}</td>
+                                    <td>{{ $data[$e]->tanggal }}</td>
+                                    <td align="left">{{ $data[$e]->keterangan }}</td>
+                                    <td align="right" > 
+                                    @if ($data[$e]->flag == 'D' or substr($data[$e]->kode,0,3) == 'INV')
+                                      {{ $data[$e]->total }}
+                                      <input type="hidden" class="debet" value="{{ $data[$e]->total }}" name="">
+                                    @else 
+                                      0
+                                      <input type="hidden" class="debet" value="0" name="">
+                                    @endif
+                                    </td>
+
+                                    <td align="right"> 
+                                    @if ($data[$e]->flag == 'K' or substr($data[$e]->kode,0,2) == 'KN' or substr($data[$e]->kode,0,3) == 'KWT' or substr($data[$e]->kode,0,3) == 'PST')
+                                      <input type="hidden" class="kredit" value="{{ $data[$e]->total }}" name="">
+                                      {{ $data[$e]->total }}
+                                    @else 
+                                      0
+                                      <input type="hidden" class="kredit" value="0" name="">
+                                    @endif
+                                    </td>
+
+                                    <td ><input type="text" name="" readonly="" class="saldo" style="text-align: right"></td>
+                            @endif 
+                           {{--  @if ($data_i[$a]->i_kode_customer == $data_p[$e]->i_kode_customer)
+                                  <td>{{ $data_p[$e]->i_nomor }}</td>
+                                  <td>{{ $data_p[$e]->i_tanggal }}</td>
+                                  <td>{{ $data_p[$e]->i_keterangan }}</td>
+                            @endif --}}
+
                       </tr>
+                        @endforeach
+                        
                     @endforeach
+                    <tr>
+                          <th colspan="3" align="right">total</th>
+                          <td><input type="text" id="total_debet" readonly="" name="" style="text-align: right;font-weight: bold;"></td>
+                          <td><input type="text" id="total_kredit" readonly="" name="" style="text-align: right;font-weight: bold;"></td>
+                          <td><input type="text" id="total_total" readonly="" name="" style="text-align: right;font-weight: bold;"></td>
+                        </tr>
                     </tbody>
 
                   </table>
@@ -142,35 +189,35 @@
 
     var table;
    
-   // table = $('#addColumn').DataTable( {
-   //    responsive: true,
-   //            searching: true,
-   //            //paging: false,
-   //            "pageLength": 10,
-   //            "language": dataTableLanguage,
-   //       dom: 'Bfrtip',
-   //       buttons: [
-   //          {
-   //                extend: 'excel',
-   //               /* messageTop: 'Hasil pencarian dari Nama : ',*/
-   //                text: ' Excel',
-   //                className:'excel',
-   //                title:'LAPORAN TARIF CABANG KOLI',
-   //                filename:'CABANGKOLI-'+a+b+c,
-   //                init: function(api, node, config) {
-   //                $(node).removeClass('btn-default'),
-   //                $(node).addClass('btn-warning'),
-   //                $(node).css({'margin-top': '-50px','margin-left': '80px'})
-   //                },
-   //                exportOptions: {
-   //                modifier: {
-   //                    page: 'all'
-   //                }
-   //            }
-              
-   //            }
-   //        ]
-   //  });
+   
+  var awal = 0;
+  $('.debet').each(function(){
+    var total = parseInt($(this).val());
+    awal += total;
+    console.log(awal);
+  });
+  $('#total_debet').val(accounting.formatMoney(awal,"",0,'.',','));
+
+  var kred = 0;
+  $('.kredit').each(function(){
+    var total = parseInt($(this).val());
+    kred += total;
+    console.log(kred);
+  });
+  $('#total_kredit').val(accounting.formatMoney(kred,"",0,'.',','));
+
+
+  var saldo = 0;
+  $('.debet').each(function(){
+    var par = $(this).parents('tr');
+    var kredit = $(par).find('.kredit').val();
+    var hasil = $(this).val() - kredit;
+    saldo += hasil;
+    $(par).find('.saldo').val(saldo);
+  })
+  $('#total_total').val(accounting.formatMoney(saldo,"",0,'.',','));
+
+
      $('.date').datepicker({
         autoclose: true,
         format: 'yyyy-mm-dd'
