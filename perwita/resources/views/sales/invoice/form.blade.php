9ca9ad510bdb1379ec7606f040521d6b303bbd70
adiyani17a <wielijarni@gmail.com>
2018-05-03 11:31:20 +0700

    asd

--- perwita/resources/views/sales/invoice/form.blade.php
+++ perwita/resources/views/sales/invoice/form.blade.php
@@ -772,31 +772,6 @@ function hitung_total_tagihan(){
                             response.data[i].deskripsi+'<input type="hidden" class="acc_penjualan" value="'+response.data[i].acc_penjualan+'" name="akun[]">',
                             response.data[i].jumlah+'<input type="hidden" value="'+response.data[i].jumlah+'" name="dd_jumlah[]">',
                             accounting.formatMoney(response.data[i].tarif_dasar, "", 2, ".",',')+'<input class="dd_harga" type="hidden" value="'+response.data[i].tarif_dasar+'" name="dd_harga[]">',
-                            accounting.formatMoney(parseFloaat(response.data[i].total)+parseFloaat(response.data[i].biaya_tambahan), "", 2, ".",',')+'<input class="dd_total" type="hidden" value="'+parseFloat(response.data[i].total)+parseFloat(response.data[i].biaya_tambahan)   +'" name="dd_total[]">',
-                            accounting.formatMoney(response.data[i].diskon, "", 2, ".",',')+'<input class="dd_diskon" type="hidden" value="'+response.data[i].diskon+'" name="dd_diskon[]">',
-                            accounting.formatMoney(response.data[i].harga_netto, "", 2, ".",',')+'<input type="hidden" class="harga_netto" value="'+response.data[i].harga_netto+'" name="harga_netto[]">',
-                            '<button type="button" onclick="hapus_detail(this)" class="btn btn-danger hapus btn-sm" title="hapus"><i class="fa fa-trash"><i></button>',
-
-                        ]).draw(false);
-                    }
-
-                    hitung();
-                    
-                    $('.customer_td').addClass('disabled');
-                    $('.grup_item_tr').addClass('disabled');
-                    $('#cb_pendapatan').attr('disabled',true);
-                    /////////////////////////////////////
-                }else if(response.jenis == 'KARGO'){
-                    for(var i = 0 ; i < response.data.length;i++){
-                        ///////////////////////////////////////
-                        index_detail+=1;
-                        table_detail.row.add([
-                            index_detail+'<p class="indexing"></p>',
-                            response.data[i].nomor+'<input class="nomor_detail" type="hidden" value="'+response.data[i].nomor+'" name="do_detail[]">',
-                            response.data[i].tanggal,
-                            response.data[i].deskripsi+'<input type="hidden" class="acc_penjualan" value="'+response.data[i].acc_penjualan+'" name="akun[]">',
-                            response.data[i].jumlah+'<input type="hidden" value="'+response.data[i].jumlah+'" name="dd_jumlah[]">',
-                            accounting.formatMoney(response.data[i].tarif_dasar, "", 2, ".",',')+'<input class="dd_harga" type="hidden" value="'+response.data[i].tarif_dasar+'" name="dd_harga[]">',
                             accounting.formatMoney(response.data[i].total, "", 2, ".",',')+'<input class="dd_total" type="hidden" value="'+response.data[i].total+'" name="dd_total[]">',
                             accounting.formatMoney(response.data[i].diskon, "", 2, ".",',')+'<input class="dd_diskon" type="hidden" value="'+response.data[i].diskon+'" name="dd_diskon[]">',
                             accounting.formatMoney(response.data[i].harga_netto, "", 2, ".",',')+'<input type="hidden" class="harga_netto" value="'+response.data[i].harga_netto+'" name="harga_netto[]">',
