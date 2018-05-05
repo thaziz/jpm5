91559a232f6b29fbe8b311f6c30d0965f07edb9c
adiWieli <dewa17a@gmail.com>
2018-05-05 17:00:32 +0700

    asd

--- perwita/resources/views/hello.blade.php
+++ perwita/resources/views/hello.blade.php
@@ -7,13 +7,17 @@
 	<title></title>
 </head>
 <body>
-	<div class="col-sm-12 ibox float-e-margins" style="background: #7dc5ef;text-align: center">
-        <img class="img" width="80" height="40" style="margin-top: 10px;" src="{{ asset('perwita/storage/app/upload/images.jpg') }}"></td>
-		<h3>Hai Kamu Memiliki Notifikasi</h3>
+	<div class="col-sm-12 ibox float-e-margins" style="background: #7dc5ef;">
+		<div style="width: 80px;height: 40px; display: inline-block; margin-top: 10px;margin-bottom: 10px; margin-left: 10px;">
+        	<img class="img" width="80" height="40" style="" src="{{ asset('perwita/storage/app/upload/images.jpg') }}">
+		</div>
+		<div style=" display: inline-block;">
+			<h3 style="display: inline-block;">Hai Kamu Memiliki Notifikasi</h3>
+		</div>
 	</div>
-	<div>
+	<div style="">
 		<p>Terdapat Kontrak {{$status}} Yang Membutuhkan Persetujuan.</p>
-		<a href="{{$kontrak}}" title="">{{$kontrak}}</a>
+		<a class="btn btn-primary" href="{{$kontrak}}" title="">Klik Disini</a>
 	</div>
 
 	
