d79ecbc07ac2710758170730768f5d58b58b0eb7
adiWieli <dewa17a@gmail.com>
2018-06-07 21:00:26 +0700

    x

--- perwita/resources/views/sales/invoice/print.blade.php
+++ perwita/resources/views/sales/invoice/print.blade.php
@@ -356,6 +356,7 @@
 					<th class="textcenter borderbottomtabel borderrighttabel" width="8%">Kuantum</th>
 					<th class="textcenter borderbottomtabel borderrighttabel" width="11%">Harga Satuan</th>
 					<th class="textcenter borderbottomtabel borderrighttabel" width="11%">Harga Bruto</th>
+					<th class="textcenter borderbottomtabel borderrighttabel" width="11%">Diskon</th>
 					<th class="textcenter borderbottomtabel" width="11%">Harga Netto</th>
 				</tr>
 				@php
@@ -368,8 +369,9 @@
 					<td class="borderbottomtabel borderrighttabel"> {{$row->id_tgl_do}} </td>
 					<td class="textleft borderbottomtabel borderrighttabel">{{$row->id_keterangan}} </td>
 					<td class="borderbottomtabel borderrighttabel">{{$row->jumlah}}&nbsp;{{$row->kode_satuan}}</td>
-					<td class="textright borderbottomtabel borderrighttabel"> {{ number_format($row->tarif_dasar, 2, ",", ".") }} </td>
-					<td class="textright borderbottomtabel borderrighttabel"> {{ number_format($row->id_harga_bruto, 2, ",", ".") }} </td>
+					<td class="textright borderbottomtabel borderrighttabel"> {{ number_format($row->tarif_dasar+$row->biaya_tambahan+$row->tarif_penerus, 2, ",", ".") }} </td>
+					<td class="textright borderbottomtabel borderrighttabel"> {{ number_format($row->id_harga_bruto+$row->biaya_tambahan, 2, ",", ".") }} </td>
+					<td class="textright borderbottomtabel borderrighttabel"> {{ number_format($row->diskon, 2, ",", ".") }} </td>
 					<td class="textright borderbottomtabel borderrighttabel"> {{ number_format($row->id_harga_netto, 2, ",", ".") }} </td>
 				</tr>
 				@endforeach
