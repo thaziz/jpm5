e2a82af366fc3c1dfba6b87f3c32467ca6f6cd82
dirga ambara <swamsid@gmail.com>
2018-07-04 11:35:41 +0700

    update jurnal 2

--- perwita/resources/views/keuangan/jurnal/index.blade.php
+++ perwita/resources/views/keuangan/jurnal/index.blade.php
@@ -172,7 +172,7 @@
 
  <!-- modal -->
 <div id="modal_tambah_akun" class="modal">
-  <div class="modal-dialog" style="width: 60%">
+  <div class="modal-dialog" style="width: 80%">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
