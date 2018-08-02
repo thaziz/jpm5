d6f394e8da87388c8f6478c7a9ef2efc1d38922a
adiyani17a <wielijarni@gmail.com>
2018-08-02 17:09:54 +0700

    asd

--- perwita/app/Http/Controllers/sales/penerimaan_penjualan_Controller.2.php
+++ perwita/app/Http/Controllers/sales/penerimaan_penjualan_Controller.2.php
@@ -1820,12 +1820,17 @@ class penerimaan_penjualan_Controller extends Controller
     public function jurnal(request $req)
     {
 
+      $head = DB::table('d_jurnal')
+                ->where('jr_detail','like','INVOICE%')
+                ->orderBy('jr_id','ASC')
+                ->get();
       $data = DB::table('d_jurnal')
                ->join('d_jurnal_dt','jrdt_jurnal','=','jr_id')
                ->join('d_akun','jrdt_acc','=','id_akun')
-               ->where('jr_ref',$req->id)
+               ->where('jr_detail','like','INVOICE%')
+               ->orderBy('jrdt_jurnal','ASC')
+               ->orderBy('jrdt_detailid','ASC')
                ->get();
-
 
       $d = [];
       $k = [];
@@ -1843,12 +1848,53 @@ class penerimaan_penjualan_Controller extends Controller
           $k[$i] = $data[$i]->jrdt_value;
         }
       }
+
+
+      for ($i=0; $i < count($data); $i++) { 
+
+        $all = 0;
+        if ($data[$i]->jrdt_detailid == '1') {
+          $piutang = $data[$i]->jrdt_value;
+        }else{
+          $all += $data[$i]->jrdt_value;
+        }
+        if ($piutang != $all) {
+          $balance[$i] = 'NO';
+        }else{
+          $balance[$i] = 'YES';
+        }
+      }
+
+      for ($i=0; $i < count($head); $i++) { 
+        $piutang = 0;
+        $biaya = 0;
+        for ($a=0; $a < count($data); $a++) { 
+          if ($head[$i]->jr_id == $data[$a]->jrdt_jurnal) {
+            
+            if ($data[$a]->jrdt_statusdk == 'D' and $data[$a]->jrdt_value > 0) {
+              $piutang+=$data[$a]->jrdt_value;
+            }else  if ($data[$a]->jrdt_statusdk == 'K' and $data[$a]->jrdt_value > 0){
+              $biaya+=$data[$a]->jrdt_value;
+            }
+            if ($i == '1257') {
+              $head[$i];
+            }
+            // dd($data[$a]);
+            if ($piutang != $biaya) {
+              $balance[$i] = 'NO';
+            }else{
+              $balance[$i] = 'YES';
+            }
+          }
+        }
+      }
+
       $d = array_values($d);
       $k = array_values($k);
 
       $d = array_sum($d);
       $k = array_sum($k);
-
+      dd($balance);
       return view('purchase.buktikaskeluar.jurnal',compact('data','d','k'));
     }
 
