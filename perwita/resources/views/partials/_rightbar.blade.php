fb27ef1771ff3186bcd682374cf07f3375104c14
foxbloodiron <ariakbar6944@gmail.com>
2018-08-08 14:07:06 +0700

    _rightbar otw 70%

--- perwita/resources/views/partials/_rightbar.blade.php
+++ perwita/resources/views/partials/_rightbar.blade.php
@@ -10,40 +10,84 @@
 		<button class="btn-close-rightbar"><i class="fa fa-times"></i></button>
 	</div>
 	<div class="rightbar-body">
-		<div class="list-skins" data-color="gray">
-			<div class="gray-bg box-skin"></div>
-			Gray
-		</div>
-		<div class="list-skins" data-color="white">
-			<div class="white-bg box-skin"></div>
-			White
-		</div>
-		<div class="list-skins" data-color="navy">
-			<div class="navy-bg box-skin"></div>
-			Navy
-		</div>
-		<div class="list-skins" data-color="dark-blue">
-			<div class="dark-blue-bg box-skin"></div>
-			Dark Blue
-		</div>
-		<div class="list-skins" data-color="blue">
-			<div class="blue-bg box-skin"></div>
-			Blue
-		</div>
-		<div class="list-skins" data-color="lazur">
-			<div class="lazur-bg box-skin"></div>
-			Lazur
-		</div>
-		<div class="list-skins" data-color="yellow">
-			<div class="yellow-bg box-skin"></div>
-			Yellow
-		</div>
-		<div class="list-skins" data-color="red">
-			<div class="red-bg box-skin"></div>
+		<div class="list-skins" data-theme="red">
+			<div class="red-bg-color box-skin"></div>
 			Red
 		</div>
-		<div class="list-skins" data-color="black">
-			<div class="black-bg box-skin"></div>
+		<div class="list-skins" data-theme="pink">
+			<div class="pink-bg-color box-skin"></div>
+			Pink
+		</div>
+		<div class="list-skins" data-theme="purple">
+			<div class="purple-bg-color box-skin"></div>
+			Purple
+		</div>
+		<div class="list-skins" data-theme="deep-purple">
+			<div class="deep-purple-bg-color box-skin"></div>
+			Deep Purple
+		</div>
+		<div class="list-skins" data-theme="indigo">
+			<div class="indigo-bg-color box-skin"></div>
+			Indigo
+		</div>
+		<div class="list-skins" data-theme="deep-blue">
+			<div class="deep-blue-bg-color box-skin"></div>
+			Deep Blue
+		</div>
+		<div class="list-skins" data-theme="blue">
+			<div class="blue-bg-color box-skin"></div>
+			Blue
+		</div>
+		<div class="list-skins" data-theme="light-blue">
+			<div class="light-blue-bg-color box-skin"></div>
+			Light Blue
+		</div>
+		<div class="list-skins" data-theme="cyan">
+			<div class="cyan-bg-color box-skin"></div>
+			Cyan
+		</div>
+		<div class="list-skins" data-theme="teal">
+			<div class="teal-bg-color box-skin"></div>
+			Teal
+		</div>
+		<div class="list-skins" data-theme="green">
+			<div class="green-bg-color box-skin"></div>
+			Green
+		</div>
+		<div class="list-skins" data-theme="light-green">
+			<div class="light-green-bg-color box-skin"></div>
+			Light Green
+		</div>
+		<div class="list-skins" data-theme="lime">
+			<div class="lime-bg-color box-skin"></div>
+			Lime
+		</div>
+		<div class="list-skins" data-theme="yellow">
+			<div class="yellow-bg-color box-skin"></div>
+			Yellow
+		</div>
+		<div class="list-skins" data-theme="amber">
+			<div class="amber-bg-color box-skin"></div>
+			Amber
+		</div>
+		<div class="list-skins" data-theme="orange">
+			<div class="orange-bg-color box-skin"></div>
+			Orange
+		</div>
+		<div class="list-skins" data-theme="deep-orange">
+			<div class="deep-orange-bg-color box-skin"></div>
+			Deep Orange
+		</div>
+		<div class="list-skins" data-theme="brown">
+			<div class="brown-bg-color box-skin"></div>
+			Brown
+		</div>
+		<div class="list-skins" data-theme="blue-grey">
+			<div class="blue-grey-bg-color box-skin"></div>
+			Blue Grey
+		</div>
+		<div class="list-skins" data-theme="black">
+			<div class="black-bg-color box-skin"></div>
 			Black
 		</div>
 	</div>
