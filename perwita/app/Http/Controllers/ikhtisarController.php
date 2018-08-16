d643e9464f4fe2c6ad863c3701cd2786d2e43ed2
adiyani17a <wielijarni@gmail.com>
2018-08-16 16:55:20 +0700

    asd

--- perwita/app/Http/Controllers/ikhtisarController.php
+++ perwita/app/Http/Controllers/ikhtisarController.php
@@ -854,13 +854,13 @@ class ikhtisarController extends Controller
 					->where('ikd_ik_id',$id)
 					->get();
 
-			$bkk = DB::table('ikhtisar_kas_detail')
+			$bpk = DB::table('ikhtisar_kas_detail')
 					->join('biaya_penerus_kas','ikd_ref','=','bpk_nota')
 					->where('ikd_ik_id',$id)
 					->orderBy('ikd_id','ikd_ik_dt','ASC')
 					->get();
 
-			$bpk = DB::table('ikhtisar_kas_detail')
+			$bkk = DB::table('ikhtisar_kas_detail')
 					->join('bukti_kas_keluar','ikd_ref','=','bkk_nota')
 					->where('ikd_ik_id',$id)
 					->orderBy('ikd_id','ikd_ik_dt','ASC')
@@ -870,18 +870,34 @@ class ikhtisarController extends Controller
 
 				$bkk[$i] = DB::table('bukti_kas_keluar')
 						 ->join('bukti_kas_keluar_detail','bkkd_bkk_id','=','bkk_id')
-						 ->select('bkkd_keterangan as keterangan')
+						 ->select('bkkd_keterangan as keterangan','bkkd_total as total','bkk_tgl as tanggal','bkkd_akun as akun')
 						 ->where('bkk_nota',$bkk[$i]->bkk_nota)
-						 ->orderBy('ikd_id','ikd_ik_dt','ASC')
+						 ->get();
+			}
+
+			for ($i=0; $i < count($bpk); $i++) { 
+				
+
+				$bpk[$i] = DB::table('biaya_penerus_kas')
+						 ->join('biaya_penerus_kas_detail','bpkd_bpk_id','=','bpk_id')
+						 ->select('bpkd_keterangan as keterangan','bpkd_tarif_penerus as total','bpkd_tanggal as tanggal','bpkd_ as tanggal','bpkd_kode_cabang_awal as cabang')
+						 ->where('bpk_nota',$bpk[$i]->bpk_nota)
 						 ->get();
 
+				for ($a=0; $a < count($bpk[$i]); $a++) { 
+					$temp = DB::table('d_akun')
+						  ->where("id_akun",'like',substr($bpk[$i][$a]->bpk_acc_biaya,0,4).'%')
+						  ->where('kode_cabang',$bpk[$i][$a]->cabang)
+						  ->first();
 
+					$bpk[$i][$a]->akun = $temp->id_akun;
+
+				}
 			}
 
 
 
 			$data_dt = array_merge($bkk,$bpk);	
-
 			$terbilang = $this->terbilang($data->ik_total,$style=3);
 
 			return view('purchase.ikhtisar_kas.outputIkhtisar',compact('terbilang','data','start','end','id','data_dt','nomor'));
