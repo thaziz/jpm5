eb4c39ce2baf92991d9c0eae740aff0332091c66
adiyani17a <wielijarni@gmail.com>
2018-08-24 11:01:22 +0700

    dfasd

--- perwita/resources/views/sales/surat_jalan_trayek/form.blade.php
+++ perwita/resources/views/sales/surat_jalan_trayek/form.blade.php
@@ -71,8 +71,8 @@
                             <tr>
                                 <td style="width:120px; padding-top: 0.4cm">Nomor</td>
                                 <td colspan="4">
-                                    <input type="text" name="ed_nomor" id="ed_nomor" class="form-control" readonly="readonly" style="text-transform: uppercase" value="{{ $data->nomor or null }}" >
-                                    <input type="hidden" name="ed_nomor_old" class="form-control" style="text-transform: uppercase" value="{{ $data->nomor or null }}" >
+                                    <input type="text" name="ed_nomor" id="ed_nomor" class="form-control" style="text-transform: uppercase" value="{{ $data->nomor or null }}" >
+                                    <input type="hidden" name="ed_nomor_old" id="ed_nomor_old" class="form-control" style="text-transform: uppercase" value="{{ $data->nomor or null }}" >
                                     <input type="hidden" class="form-control" name="_token" value="{{ csrf_token() }}" readonly="" >
                                     <input type="hidden" class="form-control" name="crud_h" class="form-control" @if ($data === null) value="N" @else value="E" @endif>
                                 </td>
@@ -80,29 +80,48 @@
                             <tr>
                                 <td style="padding-top: 0.4cm">Tanggal</td>
                                 <td>
-                                    <div class="input-group date">
-                                        <span class="input-group-addon"><i class="fa fa-calendar"></i></span><input type="text" class="form-control date" name="ed_tanggal" value="{{ $data->tanggal or  date('Y-m-d') }}">
+                                    <div class="input-group">
+                                        <span class="input-group-addon"><i class="fa fa-calendar"></i></span><input type="text" class="form-control tanggal" name="tanggal" value="{{ $data->tanggal or  date('Y-m-d') }} " onblur="ganti_nota()" >
                                     </div>
                                 </td>
                                 <td style="padding-top: 0.4cm">Filter Tanggal</td>
                                 <td>
                                     <div class="input-group ">
                                         <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
-                                        <input type="text" class="range_date form-control" name="range_date" value="{{ carbon\carbon::now()->subDays(30)->format('Y-m-d') }} - {{ carbon\carbon::now()->format('Y-m-d') }}">
+                                        <input type="text" class="range_date form-control" name="range_date" value="{{ carbon\carbon::now()->subDays(30)->format('d/m/Y') }} - {{ carbon\carbon::now()->format('d/m/Y') }}">
                                     </div>
                                 </td>
                             </tr>
-                            <tr>
-                                <td style="width:110px; padding-top: 0.4cm">Cabang</td>
-                                <td colspan="4">
-                                    <select class="form-control chosen-select-width cabang" name="cb_cabang" >
-                                    @foreach ($cabang as $row)
-                                        <option value="{{ $row->kode }}">{{ $row->kode }} - {{ $row->nama }} </option>
-                                    @endforeach
-                                    </select>
-                                    <input type="hidden" name="ed_cabang" value="{{ $data->kode_cabang or null }}" >
-                                </td>
-                            </tr>
+                            @if (Auth::user()->punyaAkses('Surat Jalan By Trayek','cabang'))
+                                <tr>
+                                    <td style="width:110px; padding-top: 0.4cm">Cabang</td>
+                                    <td colspan="4">
+                                        <select class="form-control chosen-select-width cabang" name="cb_cabang" onchange="ganti_nota()" >
+                                        @foreach ($cabang as $row)
+                                            <option @if ($row->kode == Auth::user()->kode_cabang)
+                                                selected="" 
+                                            @endif value="{{ $row->kode }}">{{ $row->kode }} - {{ $row->nama }} </option>
+                                        @endforeach
+                                        </select>
+                                        <input type="hidden" name="ed_cabang" value="{{ $data->kode_cabang or null }}" >
+                                    </td>
+                                </tr>
+                            @else
+                                <tr class="disabled">
+                                    <td style="width:110px; padding-top: 0.4cm">Cabang</td>
+                                    <td colspan="4">
+                                        <select class="form-control chosen-select-width cabang" name="cb_cabang" onchange="ganti_nota()" >
+                                        @foreach ($cabang as $row)
+                                            <option @if ($row->kode == Auth::user()->kode_cabang)
+                                                selected="" 
+                                            @endif value="{{ $row->kode }}">{{ $row->kode }} - {{ $row->nama }} </option>
+                                        @endforeach
+                                        </select>
+                                        <input type="hidden" name="ed_cabang" value="{{ $data->kode_cabang or null }}" >
+                                    </td>
+                                </tr>
+                            @endif
+                            
                             <tr>
                                 <td style="width:110px; padding-top: 0.4cm">Rute</td>
                                 <td colspan="4">
@@ -166,37 +185,7 @@
                     </tbody>
                   </table>
                 </div><!-- /.box-body -->
-                <!-- modal -->
-                <div id="modal" class="modal" >
-                  <div class="modal-dialog">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <h4 class="modal-title">Insert Nomor DO</h4>
-                  </div>
-                      <div class="modal-body">
-                            <form class="form-horizontal  kirim">
-                                <table id="table_data_do" class="table table-bordered table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th>Nomor Order</th>
-                                            <th>Tgl Order</th>
-                                            <th>Tujuan</th>
-                                            <th>Aksi</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    </tbody>
-                                </table>
-                            </form>
-                          </div>
-                          <div class="modal-footer">
-                            <button type="submit" class="btn btn-primary" id="btnsave">Save changes</button>
-                          </div>
-                    </div>
-                  </div>
-                </div>
-                  <!-- modal -->
+                
                 <div class="box-footer">
                   <div class="pull-right">
 
@@ -211,7 +200,37 @@
     </div>
 </div>
 
-
+<!-- modal -->
+<div id="modal" class="modal" >
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title">Insert Nomor DO</h4>
+  </div>
+      <div class="modal-body">
+            <form class="form-horizontal  kirim">
+                <table id="table_data_do" class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Nomor Order</th>
+                            <th>Tgl Order</th>
+                            <th>Tujuan</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="submit" class="btn btn-primary" id="btnsave">Save changes</button>
+          </div>
+    </div>
+  </div>
+</div>
+  <!-- modal -->
 
 <div class="row" style="padding-bottom: 50px;"></div>
 
@@ -223,28 +242,17 @@
 @section('extra_scripts')
 <script type="text/javascript">
 
-    $('.date').datepicker({
+    $('.tanggal').datepicker({
         format:'yyyy-mm-dd'
     });
     $('.range_date').daterangepicker({
         autoclose: true,
           "opens": "left",
           locale: {
-          format: 'YYYY-MM-DD'
+          format: 'DD/MM/YYYY'
           }
     });
     $(document).ready( function () {
-        $("input[name='ed_tanggal']").focus();
-        $("select[name='cb_cabang']").val('{{ $data->kode_cabang or ''  }}');
-        $("select[name='cb_rute']").val('{{ $data->kode_rute or ''  }}').trigger('chosen:updated');
-        $("select[name='cb_nopol']").val('{{ $data->id_kendaraan or ''  }}').trigger('chosen:updated');
-        $jml_detail = {{ $jml_detail->jumlah  or 0}};
-        if ($jml_detail > 0){
-            $("input[name='ed_nomor']").attr('readonly','readonly');
-            $("select[name='cb_rute']").prop('disabled', true);
-        }else{
-            //$("input[name='ed_nomor']").focus();
-        }
         $('#table_data').DataTable({
             "lengthChange": true,
             "ordering": true,
@@ -283,8 +291,33 @@
             for (var selector in config) {
                 $(selector).chosen(config[selector]);
             }
+        var tanggal = $('.tanggal').val();
+        var cabang  = $('.cabang').val();
+        $.ajax({
+            url:baseUrl+'/sales/surat_jalan_trayek_form/ganti_nota',
+            data:{cabang,tanggal},
+            dataType : 'json',
+            success:function(response){
+                $('#ed_nomor_old').val(response.nota);
+                $('#ed_nomor').val(response.nota);
+            }
+        });
+
     });
     
+    function ganti_nota() {
+        var tanggal = $('.tanggal').val();
+        var cabang  = $('.cabang').val();
+        $.ajax({
+            url:baseUrl+'/sales/surat_jalan_trayek_form/ganti_nota',
+            data:{cabang,tanggal},
+            dataType : 'json',
+            success:function(response){
+                $('#ed_nomor_old').val(response.nota);
+                $('#ed_nomor').val(response.nota);
+            }
+        });
+    }
     function tampil_data_do(){
         var rute = $("#cb_rute").val();
         var cabang = $(".cabang").val();
@@ -296,8 +329,6 @@
             "searching": true,
             "paging": true,
             "ordering": true,
-            processing: true,
-            serverSide: true,
             "info": false,
             "responsive": true,
             "autoWidth": false,
@@ -317,47 +348,7 @@
         });
     }
 
-    $(document).on("click","#btnadd",function(){
-        $("input[name='crud']").val('N');
-        tampil_data_do();
-        var table = $('#table_data_do').DataTable();
-        table.ajax.reload( null, false );
-        $("#modal").modal("show");
-        // $.ajax(
-        // {
-        //     url :  baseUrl + "/sales/surat_jalan_trayek/save_data",
-        //     type: "POST",
-        //     dataType:"JSON",
-        //     data : $('#form_header').serialize() ,
-        //     success: function(data, textStatus, jqXHR)
-        //     {
-        //         if(data.crud == 'N'){
-        //             if(data.result != 1){
-        //                 alert("Gagal menyimpan data!");
-        //             }else{
-        //                 $("input[name='ed_nomor']").val(data.nomor);
-        //                 $("input[name='ed_nomor_old']").val(data.nomor);
-                        
-        //             }
-        //         }else if(data.crud == 'E'){
-        //             if(data.result != 1){
-        //                 swal("Error","Can't update data, error : "+data.error,"error");
-        //             }else{
-        //                 $("input[name='ed_nomor']").val(data.nomor);
-        //                 $("input[name='ed_nomor_old']").val(data.nomor);
-                        
-        //             }
-        //         }else{
-        //              swal("Error","invalid order","error");
-        //         }
-                
-        //     },
-        //     error: function(jqXHR, textStatus, errorThrown)
-        //     {
-        //       // swal("Error!", textStatus, "error");
-        //     }
-        // });
-    });
+
 
     $(document).on("click","#btnsimpan",function(){
         $("select[name='cb_rute']").prop('disabled', false).trigger("chosen:updated");
@@ -370,21 +361,7 @@
             data : $('#form_header').serialize() ,
             success: function(data, textStatus, jqXHR)
             {
-                if(data.crud == 'N'){
-                    if(data.result != 1){
-                        alert("Gagal menyimpan data!");
-                    }else{
-                        window.location.href = baseUrl + '/sales/surat_jalan_trayek'
-                    }
-                }else if(data.crud == 'E'){
-                    if(data.result != 1){
-                        swal("Error","Can't update data, error : "+data.error,"error");
-                    }else{
-                        window.location.href = baseUrl + '/sales/surat_jalan_trayek';
-                    }
-                }else{
-                    swal("Error","invalid order","error");
-                }
+             
             },
             error: function(jqXHR, textStatus, errorThrown)
             {
@@ -413,78 +390,37 @@
     });
 
 
-    $(document).on("click","#btnsave",function(){
-        var nomor_do = [];
-        $(':checkbox:checked').each(function(i){
-          nomor_do[i] = $(this).attr("id");
-        });
-        var nomor = $("input[name='ed_nomor']").val();
-        var value = {
-            nomor : nomor,
-            nomor_do: nomor_do,
-            _token: "{{ csrf_token() }}"
-        };
-        $.ajax(
-        {
-            url : baseUrl + "/sales/surat_jalan_trayek/save_data_detail",
-            type: "POST",
-            dataType:"JSON",
-            data : value ,
-            success: function(data, textStatus, jqXHR)
-            {
-                if(data.result == 1){
-                    var table = $('#table_data').DataTable();
-                    table.ajax.reload( null, false );
-                    $("#modal").modal('hide');
-                    //$("input[name='ed_nomor']").attr('readonly','readonly');
-                    $("select[name='cb_rute']").prop('disabled', true).trigger("chosen:updated");
-                    $("select[name='cb_cabang']").attr('disabled','disabled');
-                    $("#btn_add").focus();
-                }else{
-                    alert("Gagal menyimpan data!");
-                }
+    $(document).on("click","#btnadd",function(){
+        var range_date = $('.range_date').val();
+        var cabang  = $('.cabang').val();
+        $('#table_data_do').DataTable({
+            "lengthChange": true,
+            "ordering": true,
+            "searching": false,
+            "paging": false,
+            "ordering": true,
+            "info": false,
+            "responsive": true,
+            "autoWidth": false,
+            "pageLength": 10,
+            "retrieve" : true,
+            "ajax": {
+                "url": baseUrl + "/sales/surat_jalan_trayek_form/tampil_do",
+                "type": "GET",
+                "data" : {cabang,range_date},
             },
-            
+            "columns": [
+            { "data": "nomor" },
+            { "data": "tanggal" },
+            { "data": "tujuan" },
+            { "data": "button" },
+            ]
         });
+
+        $('#modal').modal('show');
     });
 
     $(document).on( "click",".btndelete", function() {
-        var name = $(this).attr("name");
-        var id = $(this).attr("id");
-        var nomor = $("input[name='ed_nomor']").val();
-        if(!confirm("Hapus Data " +name+ " ?")) return false;
-        var value = {
-            id: id,
-            nomor: nomor,
-            _token: "{{ csrf_token() }}"
-        };
-        $.ajax({
-            type: "POST",
-            url : baseUrl + "/sales/surat_jalan_trayek/hapus_data_detail",
-            dataType:"JSON",
-            data: value,
-            success: function(data, textStatus, jqXHR)
-            {
-                if(data.result ==1){
-                    var table = $('#table_data').DataTable();
-                    if (data.jml_detail == 0) {
-                        //$("input[name='ed_nomor']").removeAttr('readonly');
-                        $("select[name='cb_cabang']").removeAttr('disabled');
-                        $("select[name='cb_rute']").prop('disabled', false).trigger("chosen:updated");
-                    }
-                    table.ajax.reload( null, false );
-                }else{
-                    //swal("Error","Data tidak bisa hapus : "+data.error,"error");
-                    alert('gagal menghapus data');
-                }
-
-            },
-            error: function(jqXHR, textStatus, errorThrown)
-            {
-                swal("Error!", textStatus, "error");
-            }
-        });
-
 
     });
 
