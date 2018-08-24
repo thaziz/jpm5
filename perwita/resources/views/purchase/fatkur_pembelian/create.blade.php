1bd92864a3d1b7278b984dd4f3c4620865ef3219
xexehon <anafrdna@gmail.com>
2018-08-24 16:43:24 +0700

    test

--- perwita/resources/views/purchase/fatkur_pembelian/create.blade.php
+++ perwita/resources/views/purchase/fatkur_pembelian/create.blade.php
@@ -206,7 +206,7 @@
                                     <td>   <select class="form-control idsup chosen-select-width1" name="supplier" required="" novalidate> 
                                             <option value=""> -- Pilih Supplier -- </option>
                                         @foreach($data['supplier'] as $supplier)
-                                            <option value="{{$supplier->idsup}}+{{$supplier->syarat_kredit}},{{$supplier->nama_supplier}}+{{$supplier->acc_hutang}}+{{$supplier->no_supplier}}"> {{$supplier->no_supplier}} - {{$supplier->nama_supplier}}</option>
+                                            <option value="{{$supplier->idsup}}+{{$supplier->syarat_kredit}}+{{$supplier->nama_supplier}}+{{$supplier->acc_hutang}}+{{$supplier->no_supplier}}"> {{$supplier->no_supplier}} - {{$supplier->nama_supplier}}</option>
                                         @endforeach
                                         </select>
                                        
@@ -887,7 +887,7 @@
                                             <td>   <select class="form-control idsup_po chosen-select-width" name="supplier_po" novalidate required=""> 
                                                     <option value=""> -- Pilih Supplier -- </option>
                                                 @foreach($data['supplier'] as $supplier)
-                                                    <option value="{{$supplier->idsup}}+{{$supplier->syarat_kredit}},{{$supplier->nama_supplier}}+{{$supplier->acc_hutang}}+{{$supplier->no_supplier}}" data-accHutang="{{$supplier->acc_hutang}}"> {{$supplier->no_supplier}} - {{$supplier->nama_supplier}}</option>
+                                                    <option value="{{$supplier->idsup}}+{{$supplier->syarat_kredit}}+{{$supplier->nama_supplier}}+{{$supplier->acc_hutang}}+{{$supplier->no_supplier}}" data-accHutang="{{$supplier->acc_hutang}}"> {{$supplier->no_supplier}} - {{$supplier->nama_supplier}}</option>
                                                 @endforeach
                                                 </select>                                        
                                             </td>
