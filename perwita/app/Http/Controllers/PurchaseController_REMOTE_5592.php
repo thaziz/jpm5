31926734d0d04ba9c249619c2a2ea8bf1e980b2d
xexehon <anafrdna@gmail.com>
2018-09-01 15:25:20 +0700

    test

--- perwita/app/Http/Controllers/PurchaseController_REMOTE_5592.php
+++ perwita/app/Http/Controllers/PurchaseController_REMOTE_5592.php
@@ -10539,7 +10539,7 @@ public function kekata($x) {
 		elseif($idjenis == '9'){ // SUBCON
 			$data['isi'] = DB::select("select * from subcon ");
 		}
-		elseif($idjenis == '1'){
+		elseif($idjenis == '1' || $idjenis == '11' || $idjenis == '13'){
 			$data['isi'] = DB::select("select * from cabang");
  		}
 
@@ -11319,6 +11319,47 @@ public function kekata($x) {
 			'fpg_keterangan' => $request->keterangan,
 			]);	
 
+		//deletenofaktur
+		$jenisbayar = $request->jenisbayarheader;
+		if($jenisbayar == '2' || $jenisbayar == '6' || $jenisbayar == '7' || $jenisbayar == '9'){
+			$idfpgdt = $request->idfpgdt;
+			$idfp = $request->idfp;
+			$pelunasan = $request->pelunasan;
+
+			$pelunasan2 = str_replace(',', '', $pelunasan);
+			
+
+			$deletefpgdt = DB::table('fpg_dt')->where('fpgdt_id' , '=' , $idfpgdt)->delete();
+			$datafaktur = DB::select("select * from faktur_pembelian where fp_idfaktur = '$idfp'");
+			$fp_pelunasan = $datafaktur[0]->fp_sisapelunasan;
+
+			$penjumlahan = (float)$fp_pelunasan + (float)$pelunasan2;
+			$updatefaktur = fakturpembelian::where('fp_idfaktur', '=' , $idfp);
+					$updatefaktur->update([
+						'fp_sisapelunasan' => $penjumlahan,
+						'fp_edit' => 'ALLOWED',
+					]);
+		}
+		else if($jenisbayar == '1'){
+			$idfpgdt = $request->idfpgdt;
+			$idfp = $request->idfp;
+			$pelunasan = $request->pelunasan;
+
+			$pelunasan2 = str_replace(',', '', $pelunasan);
+			
+
+			$deletefpgdt = DB::table('fpg_dt')->where('fpgdt_id' , '=' , $idfpgdt)->delete();
+			$datafaktur = DB::select("select * from faktur_pembelian where fp_idfaktur = '$idfp'");
+			$fp_pelunasan = $datafaktur[0]->fp_sisapelunasan;
+
+			$penjumlahan = (float)$fp_pelunasan + (float)$pelunasan2;
+			$updatefaktur = fakturpembelian::where('fp_idfaktur', '=' , $idfp);
+					$updatefaktur->update([
+						'fp_sisapelunasan' => $penjumlahan,
+						'fp_edit' => 'ALLOWED',
+					]);
+		}
+
 	
 		for($j=0;$j<count($request->nofaktur);$j++){
 			$idfp = $request->idfaktur[$j];
@@ -11490,8 +11531,6 @@ public function kekata($x) {
 				$updatefaktur->update([
 					'fp_sisapelunasan' => $penjumlahan
 				]);
-
-		return 'ok';
 	}
 
 
