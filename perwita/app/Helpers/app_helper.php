376c39a1d317c0f364eb0e52967d7b70203eeadf
adiWieli <dewa17a@gmail.com>
2018-09-04 09:21:16 +0700

    sd

--- perwita/app/Helpers/app_helper.php
+++ perwita/app/Helpers/app_helper.php
@@ -199,6 +199,7 @@
 		$k = 0;
 
 		for ($i=0; $i < count($data); $i++) { 
+			$temp = 0;
 			if ($data[$i]->jrdt_status_dka == 'D') {
 				if ($data[$i]->jrdt_value < 0) {
 					$temp = $data[$i]->jrdt_value * -1;
