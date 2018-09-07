94b33d5108fbd642cee7015942a9f8f9ed152943
xexehon <anafrdna@gmail.com>
2018-09-07 11:21:08 +0700

    test

--- perwita/app/Http/Controllers/PurchaseController_REMOTE_5592.php
+++ perwita/app/Http/Controllers/PurchaseController_REMOTE_5592.php
@@ -8794,7 +8794,7 @@ public function kekata($x) {
 	public function createformfpg() {
 
 		$data['supplier'] = DB::select("select * from supplier where status = 'SETUJU' and active = 'AKTIF'");
-		$data['jenisbayar'] = DB::select("select * from jenisbayar where idjenisbayar != '8'  and idjenisbayar != 10 and idjenisbayar != 12");
+		$data['jenisbayar'] = DB::select("select * from jenisbayar where idjenisbayar != '8'  and idjenisbayar != 10");
 
 		$cabang = session::get('cabang');
 
@@ -8938,7 +8938,7 @@ public function kekata($x) {
 			 	'mbdt_tglstatus' => null,
 		 	]);	
 
-			$bankmasuk = DB::select("select * bank_masuk where bm_idfpgb = '$idbank'");
+			$bankmasuk = DB::select("select * from bank_masuk where bm_idfpgb = '$idbank'");
 			if(count($bankmasuk) > 0) {
 				$idbm = $bankmasul[0]->bm_id;
 				DB::delete("DELETE from bank_masuk where bm_id = '$idbm'");
