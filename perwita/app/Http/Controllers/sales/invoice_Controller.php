efbf5d735b04a879bf4d5c76cedd88a0da6fff2e
adiWieli <dewa17a@gmail.com>
2018-09-29 15:41:08 +0700

    sd

--- perwita/app/Http/Controllers/sales/invoice_Controller.php
+++ perwita/app/Http/Controllers/sales/invoice_Controller.php
@@ -111,7 +111,12 @@ class invoice_Controller extends Controller
                                   }
                                 }
                               }elseif (Auth::user()->m_name = 'dev') {
-                                $a = '<button type="button" onclick="edit(\''.$data->i_nomor.'\')" data-toggle="tooltip" title="Edit" class="btn btn-success btn-xs btnedit"><i class="fa fa-pencil"></i></button>';
+                                $total = $data->i_total_tagihan + $data->i_debet - $data->i_kredit;
+                                if ($data->i_sisa_pelunasan == $total) {
+                                  if(cek_periode(carbon::parse($data->i_tanggal)->format('m'),carbon::parse($data->i_tanggal)->format('Y') ) != 0){
+                                    $a = '<button type="button" onclick="edit(\''.$data->i_nomor.'\')" data-toggle="tooltip" title="Edit" class="btn btn-success btn-xs btnedit"><i class="fa fa-pencil"></i></button>';
+                                  }
+                                }
                               }
                             }else{
 
@@ -135,7 +140,12 @@ class invoice_Controller extends Controller
                                   }
                                 }
                               }elseif (Auth::user()->m_name = 'dev') {
-                                $c = '<button type="button" onclick="hapus(\''.$data->i_nomor.'\')" class="btn btn-xs btn-danger btnhapus"><i class="fa fa-trash"></i></button>';
+                                $total = $data->i_total_tagihan + $data->i_debet - $data->i_kredit;
+                                if ($data->i_sisa_pelunasan == $total) {
+                                  if(cek_periode(carbon::parse($data->i_tanggal)->format('m'),carbon::parse($data->i_tanggal)->format('Y') ) != 0){
+                                    $c = '<button type="button" onclick="hapus(\''.$data->i_nomor.'\')" class="btn btn-xs btn-danger btnhapus"><i class="fa fa-trash"></i></button>';
+                                  }
+                                }
                               }
                             }else{
                               $c = '';
